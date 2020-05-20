95a488d13d87172775d560fcb1d7f0d6772b67d3
agapkinadiana <agapkinadiana@mail.ru>
2020-05-20 13:45:09 +0300

    05a done

---
+++ MVC - 5a/Lab5/Controllers/CResearchController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab5.Controllers
+{
+    public class CResearchController : Controller
+    {
+        public string Index()
+        {
+            string body;
+            string method = Request.HttpMethod;
+            string header = Request.Headers.ToString();
+            string uri = Request.Url.AbsoluteUri;
+            string param = Request.QueryString["param"];
+
+            using (StreamReader reader = new StreamReader(Request.InputStream))
+            {
+                body = reader.ReadToEnd();
+            }
+            return $"body: {body}; " +
+                $"header: {header}; " +
+                $"method: {method}; " +
+                $"uri: {uri}; " +
+                $"params: {param}; ";
+        }
+
+        public string C01()
+        {
+            string body;
+            string method = Request.HttpMethod;
+            string header = Request.Headers.ToString();
+            string uri = Request.Url.AbsoluteUri;
+            string param = Request.QueryString["param"];
+
+            using (StreamReader reader = new StreamReader(Request.InputStream))
+            {
+                body = reader.ReadToEnd();
+            }
+            return $"body: {body}; " +
+                $"header: {header}; " +
+                $"method: {method}; " +
+                $"uri: {uri}; " +
+                $"params: {param}; ";
+        }
+
+        public string C02()
+        {
+            string body;
+            string status = HttpContext.Response.StatusCode.ToString();
+            string header = Request.Headers.ToString();
+
+            using (StreamReader reader = new StreamReader(Request.InputStream))
+            {
+                body = reader.ReadToEnd();
+            }
+            return $"body: {body};" +
+                $"header: {header};" +
+                $"status: {status}";
+        }
+    }
+}
---
+++ MVC - 5a/Lab5/Controllers/MResearchController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab5.Controllers
+{
+    public class MResearchController : Controller
+    {
+        //ERROR
+        public string MXX()
+        {
+            return "MXX";
+        }
+
+        //M01
+        public string Index()
+        {
+            return "GET:M01";
+        }
+
+        public string M01()
+        {
+            return "GET:M01";
+        }
+
+        [Route("V2/MResearch/M01")]
+        public string V2_M01()
+        {
+            return "GET:M01";
+        }
+
+        [Route("V3/MResearch/{X:maxlength(1)}/M01")]
+        public string V3_X_M01()
+        {
+            return "GET:M01";
+        }
+
+
+        //M02
+        public string M02()
+        {
+            return "GET:M02";
+        }
+
+        [Route("V2")]
+        public string V2()
+        {
+            return "GET:M02";
+        }
+
+        [Route("V2/MResearch/M02")]
+        public string V2_M02()
+        {
+            return "GET:M02";
+        }
+
+        [Route("V2/MResearch")]
+        public string V2_M020()
+        {
+            return "GET:M02";
+        }
+
+        [Route("V3/MResearch/{X:maxlength(1)}/M02")]
+        public string V3_X_M02()
+        {
+            return "GET:M01";
+        }
+
+        //M03
+        [Route("V3")]
+        public string V3()
+        {
+            return "GET:M02";
+        }
+        [Route("V3/MResearch/{X:maxlength(1)}/M03")]
+        public string V3_X_M03()
+        {
+            return "GET:M01";
+        }
+        [Route("V3/MResearch/{X:maxlength(1)}")]
+        public string V3_X_M030()
+        {
+            return "GET:M01";
+        }
+    }
+}
